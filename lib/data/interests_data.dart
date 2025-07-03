24841e11e205acceb04b7a194b20f41471fda993
sandrasamara <sandranagawa30@gmail.com>
2025-07-03 15:54:14 +0300

    interest

---
+++ lib/data/interests_data.dart
@@ -0,0 +1,24 @@
+import '../models/interest_model.dart';
+
+class InterestsData {
+  static List<Interest> get defaultInterests => [
+    const Interest(id: 'gadgets', name: 'Gadgets', isSelected: false),
+    const Interest(id: 'plastics', name: 'Plastics', isSelected:false),
+    const Interest(id: 'shoes', name: 'Shoes', isSelected:false),
+    const Interest(id: 'school_bags', name: 'School bags', isSelected: false),
+    const Interest(id: 'clothes', name: 'Clothes', isSelected: false),
+    const Interest(id: 'cleaning_items', name: 'Cleaning items', isSelected: false),
+    const Interest(id: 'beddings', name: 'Beddings', isSelected: false),
+    const Interest(id: 'vases', name: 'Vases', isSelected: false),
+    const Interest(id: 'cutlery', name: 'Cutlery', isSelected: false),
+    const Interest(id: 'paintings', name: 'Paintings', isSelected: false),
+    const Interest(id: 'furniture', name: 'Furniture', isSelected: false),
+    const Interest(id: 'sports_items', name: 'Sports items', isSelected: false),
+    const Interest(id: 'books', name: 'Books', isSelected: false),
+    const Interest(id: 'closet_items', name: 'Closet items', isSelected: false),
+    const Interest(id: 'stationary', name: 'Stationary', isSelected: false),
+    const Interest(id: 'curtains', name: 'Curtains', isSelected: false),
+    const Interest(id: 'traveling_bags', name: 'Traveling bags', isSelected: false),
+    const Interest(id: 'carpets', name: 'Carpets', isSelected: false),
+  ];
+}
