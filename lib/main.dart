9123e9e984b6f9494280592de8123dd826cfcc3f
Mukian-Mayan <mukianmayan@gmail.com>
2025-07-04 13:50:08 +0300

    firebase link up

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/onboarding_screen.dart';
