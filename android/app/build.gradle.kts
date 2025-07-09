ed9c3800a5ae9607540c8ba3fae7c8c5b1428ef8
Mukian-Mayan <mukianmayan@gmail.com>
2025-07-09 09:18:23 +0300

    9th july commits for last meetup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,8 @@ plugins {
 android {
     namespace = "com.example.kampusmart2"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+    //ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
