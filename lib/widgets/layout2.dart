b63a4037b1d7c2a68ce525c9f6b03daf32eec2ac
Mukian-Mayan <mukianmayan@gmail.com>
2025-07-10 14:38:18 +0300

    final linkup, smooth flow, and others

---
+++ lib/widgets/layout2.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:kampusmart2/Theme/app_theme.dart';
+
+class Layout2 extends StatelessWidget {
+  final Widget? child;
+  const Layout2({super.key, this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: SizedBox(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * 0.3,
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [BoxShadow(color: AppTheme.taleBlack, offset: Offset(5, 5), blurRadius: 10)],
+                color: AppTheme.lightGrey,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(0),
+                  topRight: Radius.circular(0),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(100),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only( bottom: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppTheme.taleBlack,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(0),
+                  topRight: Radius.circular(0),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(100),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only( bottom: 18),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppTheme.primaryOrange,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(0),
+                  topRight: Radius.circular(0),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(100),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only( bottom: 40),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppTheme.deepBlue,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(0),
+                  topRight: Radius.circular(0),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(100),
+                  ),
+                ),
+              ),
+            ),
+            if (child != null) child!,
+          ],
+        ),
+      ),
+    );
+  }
+}
