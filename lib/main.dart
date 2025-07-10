38448992114301f749fa47340c9e0616951219e7
sandrasamara <sandranagawa30@gmail.com>
2025-07-10 11:03:56 +0300

    modifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:kampusmart2/screens/cart_page.dart';
 import '../screens/chats_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
