e87d7b08a8d506510acec52034fbe68bc726de57
sandrasamara <sandranagawa30@gmail.com>
2025-07-24 23:54:13 +0300

    Remove unused imports from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:kampusmart2/screens/home_page.dart';
 import 'package:provider/provider.dart';
 
 import 'firebase_options.dart';
@@ -10,14 +9,11 @@ import 'screens/welcome_screen.dart';
 import 'screens/onboarding_screen.dart';
 import 'screens/interest_screen.dart';
 import 'screens/onboarding_screen1.dart';
-import 'screens/chats_screen.dart';
 import 'screens/sellers_dashboard.dart';
 import 'screens/seller_add_product.dart';
 import 'screens/seller_sales_tracking.dart';
 import 'screens/login_or_register_page.dart';
 import 'package:kampusmart2/widgets/theme_provider.dart';
-import 'Theme/app_theme.dart';
-import 'screens/notification_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
