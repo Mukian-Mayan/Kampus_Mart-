1cb077ba98d2d973bbf6a46978e0eb851973f9d9
Mukian-Mayan <mukianmayan@gmail.com>
2025-07-29 11:26:07 +0300

    advancements

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:kampusmart2/screens/settings_page.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -64,9 +63,8 @@ class MyApp extends StatelessWidget {
       title: 'Kampus mart',
       theme: themeProvider.currentTheme,
       debugShowCheckedModeBanner: false,
-      home: SettingsPage(userRole: UserRole.buyer),
-      //home: isLoggedIn ? HomePage(userRole: userRole) : const SplashScreen(),
-      /*routes: {
+      home: isLoggedIn ? HomePage(userRole: userRole) : const SplashScreen(),
+      routes: {
         '/WelcomeScreen': (context) => const WelcomeScreen(),
         '/OnboardingScreen': (context) => const OnboardingScreen(),
         '/FourthOnboardingScreen': (context) => const FourthOnboardingScreen(),
@@ -75,7 +73,7 @@ class MyApp extends StatelessWidget {
         '/SellerDashboard': (context) => const SellerDashboardScreen(),
         '/AddProduct': (context) => const SellerAddProductScreen(),
         '/SalesTracking': (context) => const SellerSalesTrackingScreen(),
-      },*/
+      },
     );
   }
 }
